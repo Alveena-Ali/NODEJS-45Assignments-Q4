6e877a3913a01ce7338fae8503efa4a7754f4355
Alveena-Ali <163353918+Alveena-Ali@users.noreply.github.com>
2024-09-06 22:23:35 +0500

    Que 4: Famous Quote

---
+++ Main.js
@@ -0,0 +1,6 @@
+"use strict";
+// Que 4: Famous Quote: Find a quote from a famous person you admire. Print the quote and the name of its author. 
+// Your output should look something like the following, including the quotation marks:
+// Albert Einstein once said, "A person who never made a mistake never tired anything new."
+let name1 = ("Albert Einstein said");
+console.log(`${name1}, "A person who never made a mistake never tired anything new." `);
